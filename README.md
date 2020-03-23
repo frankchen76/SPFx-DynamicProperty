ebec787c85deb46388196e194950d37b26a7d532
Frank Chen <chentao76@hotmail.com>
2020-03-23 10:29:56 -0700

    updated lib to the latest. updated readme.md

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 This is a webpart to demonstrate the communication between extension and web part. 
 
+### Explanation
+The IDynamicDataSource array returned WebPartContext.dynamicDataProvider.getAvailableSources() includes all dynamic data property object. Each IDynamicDataSource has a Id to indicate each source. the Id format is "Webpart.[WebpartId].[SeqNum]" which allows you to get the source based on a Wep Part Id since the Web Part Id is predefined in manifest file. The below flow will show you how to programmatically bind to a dynamic property source: 
+* record your web part id from web part's manifest file. 
+  ![](screenshots/img02.png)
+* use `WebPartContext.dynamicDataProvider.getAvailableSources()` to enumerate all available source and find out source based on the web part id found from above step.
+* use `WebPartContext.dynamicDataProvider.registerSourceChanged(source.id, this._commandChanged)` to register a callback for dynamic property source change event. 
+  ![](screenshots/img01.png)
+* since the web part can be added later when a end user manually add that, you also need to register `WebPartContext.dynamicDataProvider.registerAvailableSourcesChanged` event to go the above process whenever a available sourc is changed. 
+
 ### Building the code
 
 ```bash
--- src/extensions/spFxCustomizer/components/Header.tsx
+++ src/extensions/spFxCustomizer/components/Header.tsx
@@ -4,117 +4,153 @@ import { IHeaderProps } from './IHeaderProps';
 import { IHeaderState } from './IHeaderState';
 import { escape } from '@microsoft/sp-lodash-subset';
 import {
-    PrimaryButton,
-    DefaultButton,
-    autobind,
-    Button,
-    Panel,
-    PanelType,
-    ChoiceGroup,
-    IChoiceGroupOption
+  PrimaryButton,
+  DefaultButton,
+  autobind,
+  Button,
+  Panel,
+  PanelType,
+  ChoiceGroup,
+  IChoiceGroupOption
 } from 'office-ui-fabric-react';
 
 
 export default class SPFxHeader extends React.Component<IHeaderProps, IHeaderState> {
-    private _selectedLocation: string;
-    constructor(props: IHeaderProps) {
-        super(props);
-        this.state = {
-            showPanel: this.props.showPanel != null && this.props.showPanel != undefined ? this.props.showPanel : false,
-            location: undefined
-        };
+  private _selectedLocation: string;
+  constructor(props: IHeaderProps) {
+    super(props);
+    this.state = {
+      showPanel: this.props.showPanel != null && this.props.showPanel != undefined ? this.props.showPanel : false,
+      location: undefined
+    };
+  }
+
+  public async componentDidMount() {
+    if (this.props.showPanel) {
+      this.setState({
+        showPanel: this.props.showPanel
+      });
     }
+  }
 
-    public async componentDidMount() {
-        if (this.props.showPanel) {
-            this.setState({
-                showPanel: this.props.showPanel
-            });
-        }
-    }
+  @autobind
+  private _changeLocationHandler(event: any): void {
+    this.setState({
+      showPanel: true
+    });
+  }
 
-    @autobind
-    private _changeLocationHandler(event: any): void {
-        this.setState({
-            showPanel: true
-        });
-    }
+  @autobind
+  private _hidePanel(): void {
+    this.setState({ showPanel: false });
 
-    @autobind
-    private _hidePanel(): void {
-        this.setState({ showPanel: false });
+  }
 
-    }
+  @autobind
+  private _savePanel(): void {
+    this.setState({ showPanel: false });
+    this.props.onLocationChanged(this._selectedLocation);
+  }
 
-    @autobind
-    private _savePanel(): void {
-        this.setState({ showPanel: false });
-        this.props.onLocationChanged(this._selectedLocation);
-    }
-
-    private _onRenderFooterContent = () => {
-        return (
-            <div>
-                <PrimaryButton onClick={this._savePanel} style={{ marginRight: '8px' }}>
-                    Save
+  private _onRenderFooterContent = () => {
+    return (
+      <div>
+        <PrimaryButton onClick={this._savePanel} style={{ marginRight: '8px' }}>
+          Save
             </PrimaryButton>
-                <DefaultButton onClick={this._hidePanel}>Cancel</DefaultButton>
-            </div>
-        );
-    }
+        <DefaultButton onClick={this._hidePanel}>Cancel</DefaultButton>
+      </div>
+    );
+  }
 
-    @autobind
-    private _onOptionChanged(target: any, option: IChoiceGroupOption) {
-        this.setState({
-            location: option.text
-        });
-        this._selectedLocation = option.text;
-    }
+  @autobind
+  private _onOptionChanged(target: any, option: IChoiceGroupOption) {
+    this.setState({
+      location: option.text
+    });
+    this._selectedLocation = option.text;
+  }
 
-    public render(): React.ReactElement<IHeaderProps> {
-        return (
-            <div className={styles.spfxHeader}>
-                <div className={styles.container}>
-                    <div className={styles.row}>
-                        <div className={styles.column}>
-                            <PrimaryButton text="Change Location"
-                                onClick={this._changeLocationHandler} />
-                            <Panel
-                                isOpen={this.state.showPanel}
-                                type={PanelType.smallFixedFar}
-                                onDismiss={this._hidePanel}
-                                headerText="Change Location"
-                                closeButtonAriaLabel="Close"
-                                onRenderFooterContent={this._onRenderFooterContent}
-                            >
-                                <ChoiceGroup
-                                    options={[
-                                        {
-                                            key: 'Redmond',
-                                            text: 'Redmond'
-                                        },
-                                        {
-                                            key: 'Seattle',
-                                            text: 'Seattle'
-                                            //checked: true
-                                        },
-                                        {
-                                            key: 'Sammamish',
-                                            text: 'Sammamish'
-                                            //disabled: true
-                                        }
-                                    ]}
-                                    label="Pick one"
-                                    required={true}
-                                    onChange={this._onOptionChanged}
-                                    defaultSelectedKey={this.state.location}
-                                    selectedKey={this.state.location}
-                                />
-                            </Panel>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  public render(): React.ReactElement<IHeaderProps> {
+    return (
+      <div className={styles.spfxHeader1}>
+        <PrimaryButton text="Change Location"
+          onClick={this._changeLocationHandler} />
+        <Panel
+          isOpen={this.state.showPanel}
+          type={PanelType.smallFixedFar}
+          onDismiss={this._hidePanel}
+          headerText="Change Location"
+          closeButtonAriaLabel="Close"
+          onRenderFooterContent={this._onRenderFooterContent}
+        >
+          <ChoiceGroup
+            options={[
+              {
+                key: 'Redmond',
+                text: 'Redmond'
+              },
+              {
+                key: 'Seattle',
+                text: 'Seattle'
+                //checked: true
+              },
+              {
+                key: 'Sammamish',
+                text: 'Sammamish'
+                //disabled: true
+              }
+            ]}
+            label="Pick one"
+            required={true}
+            onChange={this._onOptionChanged}
+            defaultSelectedKey={this.state.location}
+            selectedKey={this.state.location}
+          />
+        </Panel>
+      </div>
+      // <div className={styles.spfxHeader}>
+      //     <div className={styles.container}>
+      //         <div className={styles.row}>
+      //             <div className={styles.column}>
+      //                 <PrimaryButton text="Change Location"
+      //                     onClick={this._changeLocationHandler} />
+      //                 <Panel
+      //                     isOpen={this.state.showPanel}
+      //                     type={PanelType.smallFixedFar}
+      //                     onDismiss={this._hidePanel}
+      //                     headerText="Change Location"
+      //                     closeButtonAriaLabel="Close"
+      //                     onRenderFooterContent={this._onRenderFooterContent}
+      //                 >
+      //                     <ChoiceGroup
+      //                         options={[
+      //                             {
+      //                                 key: 'Redmond',
+      //                                 text: 'Redmond'
+      //                             },
+      //                             {
+      //                                 key: 'Seattle',
+      //                                 text: 'Seattle'
+      //                                 //checked: true
+      //                             },
+      //                             {
+      //                                 key: 'Sammamish',
+      //                                 text: 'Sammamish'
+      //                                 //disabled: true
+      //                             }
+      //                         ]}
+      //                         label="Pick one"
+      //                         required={true}
+      //                         onChange={this._onOptionChanged}
+      //                         defaultSelectedKey={this.state.location}
+      //                         selectedKey={this.state.location}
+      //                     />
+      //                 </Panel>
+      //             </div>
+      //         </div>
+      //     </div>
+      // </div>
+    );
+  }
 }
